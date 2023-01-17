a55fcf41a3b85190095318ae286c4877de8d739c
Maryna Dzuh <maryna.dzuh@gmail.com>
2023-01-17 10:54:09 +0200

    upd Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,7 @@ const initialValues = {
 };
 const schema = yup.object().shape({
     name: yup.string().required().matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,"Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" ),
-number:yup.string().required().phone('UA', true, `number is invalid`)
+number:yup.string().required().phone('UA', true, `phone number must be 12 or 10 digits and can contain spaces, dashes, parentheses and can start with +`)
 })
 
 export class ContactForm extends Component  {
